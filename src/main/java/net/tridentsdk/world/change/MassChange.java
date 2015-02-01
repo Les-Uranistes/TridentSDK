161468b5c93a50d00e45a03f0d5d4dc13d9fe6fa
Tapette <donotmail@example.com>
2015-02-01 16:40:19 -0600

    reformaté affichiers en /world/change

--- src/main/java/net/tridentsdk/world/change/MassChange.java
+++ src/main/java/net/tridentsdk/world/change/MassChange.java
@@ -35,6 +35,7 @@ public interface MassChange {
      * @param y  the y of the block to set
      * @param z  the z of the block to set
      * @param id the block id to change the block to
+     *
      * @throws java.lang.IllegalStateException if this change has already been committed
      */
     void setBlock(int x, int y, int z, int id) throws IllegalStateException;
@@ -46,6 +47,7 @@ public interface MassChange {
      * @param y         the y of the block to set
      * @param z         the z of the block to set
      * @param substance the substance to change the block to
+     *
      * @throws java.lang.IllegalStateException if this change has already been committed
      */
     void setBlock(int x, int y, int z, Substance substance) throws IllegalStateException;
@@ -58,6 +60,7 @@ public interface MassChange {
      * @param z    the z of the block to set
      * @param id   the block id to change the block to
      * @param data the data value to give the new block
+     *
      * @throws java.lang.IllegalStateException if this change has already been committed
      */
     void setBlock(int x, int y, int z, int id, byte data) throws IllegalStateException;
@@ -70,21 +73,21 @@ public interface MassChange {
      * @param z         the z of the block to set
      * @param substance the substance to change the block to
      * @param data      the data value to give the new block
+     *
      * @throws java.lang.IllegalStateException
      */
-    void setBlock(int x, int y, int z, Substance substance, byte data)
-            throws IllegalStateException;
+    void setBlock(int x, int y, int z, Substance substance, byte data) throws IllegalStateException;
 
     /**
      * Add a change to make at when this change is committed
      *
      * @param coords the location to set, values will be rounded
      * @param id     the id to change the block to
+     *
      * @throws IllegalArgumentException if coords are not from the same world as this change
      * @throws IllegalStateException    if this change has already been committed
      */
-    void setBlock(Position coords, int id)
-            throws IllegalArgumentException, IllegalStateException;
+    void setBlock(Position coords, int id) throws IllegalArgumentException, IllegalStateException;
 
     /**
      * Add a change to make at when this change is committed
@@ -92,22 +95,22 @@ public interface MassChange {
      * @param coords the location to set, values will be rounded
      * @param id     the id to change the block to
      * @param data   the data value to change the block to
+     *
      * @throws IllegalArgumentException if coords are not from the same world as this change
      * @throws IllegalStateException    if this change has already been committed
      */
-    void setBlock(Position coords, int id, byte data)
-            throws IllegalArgumentException, IllegalStateException;
+    void setBlock(Position coords, int id, byte data) throws IllegalArgumentException, IllegalStateException;
 
     /**
      * Add a change to make at when this change is committed
      *
      * @param coords    the location to set, values will be rounded
      * @param substance the substance to change the block to
+     *
      * @throws IllegalArgumentException if coords are not from the same world as this change
      * @throws IllegalStateException    if this change has already been committed
      */
-    void setBlock(Position coords, Substance substance)
-            throws IllegalArgumentException, IllegalStateException;
+    void setBlock(Position coords, Substance substance) throws IllegalArgumentException, IllegalStateException;
 
     /**
      * Add a change to make at when this change is committed
@@ -115,11 +118,11 @@ public interface MassChange {
      * @param coords    the location to set, values will be rounded
      * @param substance the id to change the block to
      * @param data      the data value to change the block to
+     *
      * @throws IllegalArgumentException if coords are not from the same world as this change
      * @throws IllegalStateException    if this change has already been committed
      */
-    void setBlock(Position coords, Substance substance, byte data)
-            throws IllegalArgumentException, IllegalStateException;
+    void setBlock(Position coords, Substance substance, byte data) throws IllegalArgumentException, IllegalStateException;
 
     /**
      * Sends the block changes to the clients and does lighting recalculations
@@ -127,6 +130,7 @@ public interface MassChange {
      * <p>This change will be rendered useless after this method is called, and should be discarded</p>
      *
      * @return Whether or not the changes were successful.
+     *
      * @throws java.lang.IllegalStateException if this change has already been committed
      */
     boolean commitChanges() throws IllegalStateException;
