69cd74be3913b77f4558b0e4da1a1b3c2c703139
Tapette <donotmail@example.com>
2015-02-01 01:37:35 -0600

    ignoré

--- src/main/java/net/tridentsdk/base/BlockSnapshot.java
+++ src/main/java/net/tridentsdk/base/BlockSnapshot.java
@@ -29,7 +29,7 @@ import javax.annotation.concurrent.Immutable;
  * @author The TridentSDK Team
  */
 @Immutable
-public class BlockSnapshot {
+public final class BlockSnapshot {
     private final Coordinates location;
     private final Substance material;
     private final byte data;
